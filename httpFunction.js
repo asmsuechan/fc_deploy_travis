339a9c7cc6a9062da0502dba33e31716bfff4e5a
asmsuechan <suenagaryoutaabc@gmail.com>
2018-07-31 16:34:06 +0900

    Add lint

--- httpFunction.js
+++ httpFunction.js
@@ -1,5 +1,5 @@
-const { hook } = require('fc-helper')
+const { hook } = require('fc-helper');
 
 exports.handler = hook(async (context) => {
-  context.body = 'hello world!\n'
-})
+  context.body = 'hello world!\n';
+});
